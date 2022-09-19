1cf34ac69eea3c225f981fd247ac9751dbb5b1a5
Downzin <101472001+Downzin01@users.noreply.github.com>
2022-09-19 00:34:32 -0300

    ✅

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+
+export const Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O bannner principal da página do Organo"/>
+        </header>
+    )
+}
